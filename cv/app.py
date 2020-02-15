33d103c5c0a72157ae7ac51edce2a9362dc57ed5
gypang <gypang24@gmail.com>
2020-02-15 15:13:29 -0800

    Take out stuff i dont want

--- cv/app.py
+++ cv/app.py
@@ -52,11 +52,10 @@ class DetectProcess(multiprocessing.Process):
 
         self._stop_tracking_frames = 15  # 0.5 sec
 
-        self._obj_detect = edgeiq.ObjectDetection(
-                "alwaysai/mobilenet_ssd")
-        self._obj_detect.load(engine=self._engine)
-        self._tracker = edgeiq.CentroidTracker(
-                deregister_frames=self._stop_tracking_frames)
+        self._obj_detect = edgeiq.PoseEstimation(
+                "alwaysai/human-pose")
+        self._obj_detect.load(engine=self._engine, accelerator=edgeiq.Accelerator.MYRIAD)
+
         self._loaded_model = self._obj_detect.model_id
 
         print("Loaded model:\n{}\n".format(self._obj_detect.model_id))
@@ -64,31 +63,20 @@ class DetectProcess(multiprocessing.Process):
         print("Accelerator: {}\n".format(self._obj_detect.accelerator))
         print("Labels:\n{}\n".format(self._obj_detect.labels))
 
+    # here is where detection is run
     def _run_detection(self):
         while True:
             frame = self._frame_queue.get()
             if isinstance(frame, str) and frame == 'stop':
                 break
 
-            results = self._obj_detect.detect_objects(
-                    frame, confidence_level=.5)
-
-            people = edgeiq.filter_predictions_by_label(
-                    results.predictions, ['person'])
-            tracked_people = self._tracker.update(people)
-
-            predictions = []
-            for (object_id, prediction) in tracked_people.items():
-                new_label = 'person {}'.format(object_id)
-                prediction.label = new_label
-                predictions.append(prediction)
+            results = self._obj_detect.estimate(frame)
 
             results = {
-                    "duration": results.duration,
-                    "predictions": people,
-                    "model_id": self._loaded_model,
-                    "tracked_people": tracked_people
-                    }
+                "duration": results.duration,
+                "model_id": self._loaded_model,
+            }
+
             self._results_queue.put(results)
 
     def run(self):
@@ -97,99 +85,6 @@ class DetectProcess(multiprocessing.Process):
         except Exception as e:
             tb = traceback.format_exc()
             self._error_queue.put((e, tb))
-
-
-class CameraStatsTracker:
-    """Track camera stats and periodically update the server."""
-    def __init__(self, server_comm, update_period_s):
-        self._server_comm = server_comm
-        self._update_period_s = update_period_s
-        self._last_update_time = None
-
-    def update_stats(self, fps, inf_time):
-        if self._server_comm is not None:
-            # skip send if enough time hasn't passed
-            if self._last_update_time is not None:
-                elapsed_seconds = (
-                        datetime.datetime.now() - self._last_update_time
-                        ).total_seconds()
-                if elapsed_seconds < self._update_period_s:
-                    return
-
-            fps = '{:.2f}'.format(fps)
-            inf_time = '{:.3f}'.format(inf_time)
-            self._server_comm.send_update_camera_stats(fps, inf_time)
-            self._last_update_time = datetime.datetime.now()
-
-
-class VideoStateTracker:
-    """Track video recording state and detected frames per person."""
-    def __init__(self, frame_count_thresh):
-        self._frame_count_thresh = frame_count_thresh
-
-        if not os.path.exists(database.VIDEO_DIR):
-            os.makedirs(database.VIDEO_DIR)
-
-        self.reset()
-
-    def reset(self):
-        self.event_active = False
-
-        self._valid = False
-        self._file_path = None
-        self._date = None
-        self._time = None
-        self._tracked_people = {}
-
-    def start(self):
-        self.event_active = True
-        if not self._valid:
-            self._date = time.strftime('%Y-%m-%d')
-            self._time = time.strftime('%H:%M:%S')
-            filename = "person-{}-{}.mp4".format(self._date, self._time)
-            self._file_path = os.path.join(database.VIDEO_DIR, filename)
-            self._valid = True
-
-    def update(self, tracked_people):
-        if not self._valid:
-            raise AssertionError('VideoStateTracker.update(): not valid!')
-        for id, obj in tracked_people.items():
-            if id not in self._tracked_people:
-                self._tracked_people[id] = {'prediction': obj, 'num_frames': 1}
-            else:
-                self._tracked_people[id]['num_frames'] += 1
-
-    @property
-    def num_people(self):
-        if not self._valid:
-            raise AssertionError('VideoStateTracker.num_people: not valid!')
-        num_people = 0
-        for id, info in self._tracked_people.items():
-            if (self._tracked_people[id]['num_frames'] >
-                    self._frame_count_thresh):
-                num_people += 1
-        return num_people
-
-    @property
-    def file_path(self):
-        if not self._valid:
-            raise AssertionError('VideoStateTracker.file_path: not valid!')
-        return self._file_path
-
-    @property
-    def date(self):
-        if not self._valid:
-            raise AssertionError('VideoStateTracker.date: not valid!')
-        return self._date
-
-    @property
-    def time(self):
-        if not self._valid:
-            raise AssertionError('VideoStateTracker.time: not valid!')
-        return self._time
-
-    def stop(self):
-        self.event_active = False
 
 
 def writer_callback(file_path, event):
@@ -215,7 +110,6 @@ class App:
         self._last_frame = None
 
         self._fps = edgeiq.FPS()
-        self._video_tracker = VideoStateTracker(self._frame_count_thresh)
         self._video_complete_event = threading.Event()
 
         # Queues for interacting with detection process
@@ -228,7 +122,6 @@ class App:
         self._streamer = None
         self._server_comm = None
         self._video_writer = None
-        self._camera_stats_tracker = None
 
     def setup(self):
         self._video_stream = edgeiq.WebcamVideoStream(cam=self._cam).start()
@@ -247,8 +140,6 @@ class App:
             self._server_comm = client.ServerComm()
             self._server_comm.setup()
 
-        self._camera_stats_tracker = CameraStatsTracker(self._server_comm, 5)
-
         self._video_writer = edgeiq.EventVideoWriter(
                 pre_roll=self._pre_roll,
                 post_roll=self._post_roll,
@@ -264,21 +155,21 @@ class App:
         except queue.Empty:
             results = self._prev_results
 
-        if len(results["tracked_people"]):
-            if not self._video_tracker.event_active:
-                self._video_tracker.start()
-                self._video_writer.start_event(
-                        output_path=self._video_tracker.file_path,
-                        callback_function=writer_callback,
-                        callback_args=(
-                            self._video_tracker.file_path,
-                            self._video_complete_event))
+        # if len(results["tracked_people"]):
+        #     if not self._video_tracker.event_active:
+        #         self._video_tracker.start()
+        #         self._video_writer.start_event(
+        #                 output_path=self._video_tracker.file_path,
+        #                 callback_function=writer_callback,
+        #                 callback_args=(
+        #                     self._video_tracker.file_path,
+        #                     self._video_complete_event))
 
-            self._video_tracker.update(results["tracked_people"])
-        else:
-            if self._video_tracker.event_active:
-                self._video_writer.finish_event()
-                self._video_tracker.stop()
+        #     self._video_tracker.update(results["tracked_people"])
+        # else:
+        #     if self._video_tracker.event_active:
+        #         self._video_writer.finish_event()
+        #         self._video_tracker.stop()
 
         self._prev_results = results
         return results
@@ -313,47 +204,6 @@ class App:
         if not self._use_streamer:
             self._server_comm.send_notify_db_update()
 
-    def _update_streamer(self, frame, results):
-        """Update the Streamer if enabled."""
-        if not self._use_streamer:
-            return
-
-        # FIXME: This is required since modifying the frame for
-        # the streamer also modifies for the writer. Simple tests
-        # show that once in the queue, the object is no longer a
-        # reference so I'm not sure why that happens.
-        frame = frame.copy()
-
-        text = ["Model: {}".format(results["model_id"])]
-        text.append("Inference time: {:1.3f} s".format(
-            results["duration"]))
-
-        if self._video_writer.state != 'Idle':
-            # Add recording circle to image
-            cv2.circle(frame, (620, 20), 15, (0, 0, 255), -1)
-
-        text.append("")
-        text.append("Videos:")
-        with database.Database() as db:
-            videos = db.get_all(organize_by_date=True)
-
-        for date in sorted(videos, reverse=True):
-            text.append('<b>{}</b>'.format(date))
-            for video in videos[date]:
-                if video['num_people'] == 1:
-                    text.append('- {} - 1 person'.format(video['time']))
-                else:
-                    text.append('- {} - {} people'.format(
-                        video['time'], video['num_people']))
-
-        text.append("")
-        text.append("Objects:")
-        for prediction in results["predictions"]:
-            text.append("{}: {:2.2f}%".format(
-                prediction.label, prediction.confidence * 100))
-
-        self._streamer.send_data(frame, text)
-
     def _check_for_errors(self):
         try:
             error, traceback = self._error_queue.get_nowait()
@@ -386,21 +236,12 @@ class App:
                     # frame, results["predictions"], colors=self._default_color)
             self._video_writer.update(frame)
 
-            if not self._use_streamer:
-                web_frame = frame.copy()
-                # if self._video_writer.state != 'Idle':
-                    # Add recording circle to image
-                    # cv2.circle(web_frame, (620, 20), 15, (0, 0, 255), -1)
-                self._server_comm.send_frame(web_frame)
+            web_frame = frame.copy()
+            self._server_comm.send_frame(web_frame)
 
             self._handle_video_complete_event()
 
-            self._update_streamer(frame, results)
-
             self._fps.update()
-
-            self._camera_stats_tracker.update_stats(
-                    self._fps.compute_fps(), results['duration'])
 
             self._check_for_errors()
 
@@ -420,9 +261,6 @@ class App:
 
         # Handle any videos completed by the writer close
         self._handle_video_complete_event()
-
-        if self._streamer:
-            self._streamer.close()
 
         if self._detect_process:
             self._frame_queue.put('stop')
@@ -452,12 +290,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.engine == 'DNN':
-        engine = edgeiq.Engine.DNN
-    elif args.engine == 'DNN_OPENVINO':
-        engine = edgeiq.Engine.DNN_OPENVINO
-    else:
-        raise ValueError('Unknown engine! ({})'.format(args.engine))
+    engine = edgeiq.Engine.DNN_OPENVINO
 
     app = App(
             cam=args.camera, use_streamer=args.use_streamer,
